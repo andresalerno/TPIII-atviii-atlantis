2a5416e7b930cbe88c1e218242fb21b43f1dbf64
andresalerno <andresalerno22@gmail.com>
2025-05-15 16:31:03 -0300

    feat: definindo os setters na entidade cliente

--- src/ts/modelos/cliente.ts
+++ src/ts/modelos/cliente.ts
@@ -21,14 +21,30 @@ export default class Cliente {
     }
 
     public get Nome() { return this.nome }
-    public get NomeSocial() { return this.nomeSocial }
-    public get DataNascimento() { return this.dataNascimento }
-    public get DataCadastro() { return this.dataCadastro }
-    public get Telefones() { return this.telefones }
-    public get Endereco() { return this.endereco }
-    public get Documentos() { return this.documentos }
-    public get Dependentes() { return this.dependentes }
-    public get Titular() { return this.titular }
+    public set Nome(nome: string) { this.nome = nome }
 
+    public get NomeSocial() { return this.nomeSocial }
+    public set NomeSocial(nomeSocial: string) { this.nomeSocial = nomeSocial }
+
+    public get DataNascimento() { return this.dataNascimento }
+    public set DataNascimento(dataNascimento: Date) { this.dataNascimento = dataNascimento }
+
+    public get DataCadastro() { return this.dataCadastro }
+    public set DataCadastro(dataCadastro: Date) { this.dataCadastro = dataCadastro }
+
+    public get Telefones() { return this.telefones }
+    public set Telefones(telefones: Telefone[]) { this.telefones = telefones }
+
+    public get Endereco() { return this.endereco }
     public set Endereco(endereco: Endereco) { this.endereco = endereco }
+
+    public get Documentos() { return this.documentos }
+    public set Documentos(documentos: Documento[]) { this.documentos = documentos }
+
+    public get Dependentes() { return this.dependentes }
+    public set Dependentes(dependentes: Cliente[]) { this.dependentes = dependentes }
+
+    public get Titular() { return this.titular }
+    public set Titular(titular: Cliente) { this.titular = titular }
+    
 }
